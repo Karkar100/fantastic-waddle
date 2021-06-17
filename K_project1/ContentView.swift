280d8f3a88cf5906cce59b38359124963f5b1110
KarKar100 <morkiral@gmail.com>
2021-06-17 15:13:47 +0300

    Initial project version

---
+++ K_project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  K_project1
+//
+//  Created by Diana on 15.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ K_project1/K_project1App.swift
@@ -0,0 +1,17 @@
+//
+//  K_project1App.swift
+//  K_project1
+//
+//  Created by Diana on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct K_project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
